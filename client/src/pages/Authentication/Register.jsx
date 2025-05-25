2b1192bd5b00edd4e9b6573e07e4e17ebc0ccd3f
Mintu Sikder <mintusikder15@gmail.com>
2025-05-25 21:58:21 +0600

    added firebase

--- client/src/pages/Authentication/Register.jsx
+++ client/src/pages/Authentication/Register.jsx
@@ -1,23 +1,45 @@
 import Lottie from "lottie-react";
 import React from "react";
-import registerLottie from "../../assets/register.json"
+import registerLottie from "../../assets/register.json";
 const Register = () => {
+  const handelRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <Lottie style={{width : "200px"}} animationData={registerLottie} loop={true}> </Lottie>
-
+          <Lottie
+            style={{ width: "200px" }}
+            animationData={registerLottie}
+            loop={true}
+          >
+            {" "}
+          </Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <h1 className="text-5xl font-bold">Register now!</h1>
-            <form className="fieldset">
+            <form onSubmit={handelRegister} className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                className="input"
+                name="email"
+                placeholder="Email"
+              />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-        
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
+
               <button className="btn btn-neutral mt-4">Login</button>
             </form>
           </div>
