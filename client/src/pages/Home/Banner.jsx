bd73e0471e0fb5804feae6d8626ae37756ee48bf
Mintu Sikder <mintusikder15@gmail.com>
2025-05-27 20:58:09 +0600

    added motion text

---
+++ client/src/pages/Home/Banner.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { motion } from "motion/react";
+const Banner = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <motion.h1
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, transition: { duration: 1 } }}
+            className="text-5xl font-bold"
+          >
+            Latest{" "}
+            <motion.span
+              animate={{
+                color: ["#ff5733", "#fff033 ", "#33ff3c ", "#33ffe9"],
+                transition: { duration: 4, repeat: Infinity },
+              }}
+            >
+              Job
+            </motion.span>{" "}
+            For You!
+          </motion.h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            wef
+            <Banner></Banner>
         </div>
     );
 };
