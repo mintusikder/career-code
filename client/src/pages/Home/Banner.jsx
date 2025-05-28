1007e7835e72cef57181cae79464ae1137c5b0c7
Mintu Sikder <mintusikder15@gmail.com>
2025-05-28 15:55:36 +0600

    added server site

--- client/src/pages/Home/Banner.jsx
+++ client/src/pages/Home/Banner.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { motion } from "motion/react";
+import team1 from "../../assets/team/t1.jpg";
+import team2 from "../../assets/team/t2.jpg";
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
+        <div className="flex-1">
+          <motion.img
+            animate={{
+              y: [100, 150,100],
+            }}
+            transition={{ duration: 5, repeat: Infinity }}
+            src={team1}
+            className="max-w-sm border-blue-600 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          />
+          <motion.img
+            animate={{
+              x: [100, 150,100],
+            }}
+            transition={{ duration: 10, delay:3, repeat: Infinity }}
+            src={team2}
+            className="max-w-sm border-blue-600 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          />
+        </div>
+        <div className="flex-1">
           <motion.h1
             initial={{ scale: 0 }}
             animate={{ scale: 1, transition: { duration: 1 } }}
